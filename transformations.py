6a7af2ca6cf8d464203f2f6d96142f27c3ec87fc
James Hepburn <jhepb059@uottawa.ca>
2026-02-13 10:53:23 -0700

    Phase 6: Mix Columns

--- transformations.py
+++ transformations.py
@@ -1,12 +1,13 @@
 '''
-transformations.py
-    - add_round_key(state, round_key)
-    - sub_bytes(state)
-    - shift_rows(state)
-    - mix_columns(state)
+Implements the core AES state transformations.
+This module provides functions that operate on the 4x4 state matrix,
+including AddRoundKey, SubBytes, ShiftRows, and MixColumns.
+These transformations implement the round operations of AES
+as defined in FIPS-197 and are used in every encryption round.
 '''
 
 from constants import S_BOX
+from gf import gf_multiply
 
 # Performs the AES AddRoundKey transformation.
 def add_round_key (state, round_key):
@@ -29,4 +30,19 @@ def shift_rows (state):
     state [1][0], state [1][1], state [1][2], state [1][3] = state [1][1], state [1][2], state [1][3], state [1][0]
     state [2][0], state [2][1], state [2][2], state [2][3] = state [2][2], state [2][3], state [2][0], state [2][1]
     state [3][0], state [3][1], state [3][2], state [3][3] = state [3][3], state [3][0], state [3][1], state [3][2] 
+    return state
+
+# Performs the AES MixColumns step on the 4x4 state matrix.
+def mix_columns (state):
+    for i in range (4):
+        a = state [0][i]
+        b = state [1][i]
+        c = state [2][i]
+        d = state [3][i]
+
+        state [0][i] = gf_multiply (a, 2) ^ gf_multiply (b, 3) ^ c ^ d
+        state [1][i] = a ^ gf_multiply (b, 2) ^ gf_multiply (c, 3) ^ d
+        state [2][i] = a ^ b ^ gf_multiply (c, 2) ^ gf_multiply (d, 3)
+        state [3][i] = gf_multiply (a, 3) ^ b ^ c ^ gf_multiply (d, 2)
+
     return state
