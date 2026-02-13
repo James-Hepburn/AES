7daa8fac145bb45512168a6706f7b436211df673
James Hepburn <jhepb059@uottawa.ca>
2026-02-13 10:39:11 -0700

    Phase 3: Sub Bytes

--- transformations.py
+++ transformations.py
@@ -6,10 +6,20 @@ transformations.py
     - mix_columns(state)
 '''
 
+from constants import S_BOX
+
 # Performs the AES AddRoundKey transformation.
 def add_round_key (state, round_key):
     for i in range (4):
         for j in range (4):
             state [i][j] ^= round_key [i][j]
+
+    return state
+
+# Performs the AES SubBytes step on the given 4x4 state matrix.
+def sub_bytes (state):
+    for i in range (4):
+        for j in range (4):
+            state [i][j] = S_BOX [state [i][j]]
 
     return state
