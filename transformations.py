d352c4743171d63b9a5370d97288adb3eb5364ee
James Hepburn <jhepb059@uottawa.ca>
2026-02-13 10:33:29 -0700

    Phase 2: Add Round Key

--- transformations.py
+++ transformations.py
@@ -5,3 +5,11 @@ transformations.py
     - shift_rows(state)
     - mix_columns(state)
 '''
+
+# Performs the AES AddRoundKey transformation.
+def add_round_key (state, round_key):
+    for i in range (4):
+        for j in range (4):
+            state [i][j] ^= round_key [i][j]
+
+    return state
